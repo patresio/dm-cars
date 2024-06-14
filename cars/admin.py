45b94fa0e49efa93df213e6b672d13a7e8ed3121
patresio <patresio@gmail.com>
2024-06-13 21:18:47 -0300

    models aula 39

--- cars/admin.py
+++ cars/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 
-from cars.models import Car
+from cars.models import Brand, Car
 
 
 # Register your models here.
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
 class CarAdmin(admin.ModelAdmin):
     list_display = ("model", "brand", "factory_year", "model_year", "value")
     search_fields = ("model", "brand")
 
 
 admin.site.register(Car, CarAdmin)
+admin.site.register(Brand, BrandAdmin)
