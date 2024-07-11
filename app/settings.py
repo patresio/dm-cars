c41b66e526d68c039a1c5cfc5ba21f76942e50e9
patresio <patresio@gmail.com>
2024-07-11 15:21:25 -0300

    uwsgi and static configuration

--- app/settings.py
+++ app/settings.py
@@ -125,8 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles" / "static"
+STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
