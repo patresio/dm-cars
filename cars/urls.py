e729e8c9b4874568280a4ebbb53d822bf0c3d2ba
patresio <patresio@gmail.com>
2024-07-11 13:48:19 -0300

    urls cars adjusting

--- cars/urls.py
+++ cars/urls.py
@@ -13,7 +13,7 @@ app_name = "car"
 urlpatterns = [
     path("", CarListView.as_view(), name="list"),
     path("new/", NewCarCreateView.as_view(), name="new"),
-    path("<int:pk>", CarDetailView.as_view(), name="detail"),
-    path("<int:pk>/update/", CarUpdateView.as_view(), name="update"),
-    path("<int:pk>/delete/", CarDeleteView.as_view(), name="delete"),
+    path("car/<int:pk>", CarDetailView.as_view(), name="detail"),
+    path("car/<int:pk>/update/", CarUpdateView.as_view(), name="update"),
+    path("car/<int:pk>/delete/", CarDeleteView.as_view(), name="delete"),
 ]
